9187e767e081ecee05492986acebfe78e3738b69
bes <rtvafk@i.ua>
2022-04-14 17:12:07 +0300

    templates for head header footer

--- resources/js/gtag.js
+++
@@ -1,8 +0,0 @@
-    // google analitycs
-
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-SEP1T05Z5V');
-
---
+++ resources/views/partials/head.blade.php
@@ -0,0 +1,30 @@
+<head>
+
+    <!-- Basic -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Электровелосипеды под ключ</title>
+
+    <meta name="keywords" content="HTML5 Template">
+    <meta name="description" content="Porto - Responsive HTML5 Template">
+    <meta name="author" content="bessmertniy">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
+
+    <link id="googleFonts" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800%7COpen+Sans:400,700,800&amp;display=swap" rel="stylesheet" type="text/css">
+    <!-- Theme CSS && Vendor CSS -->
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
+
+
+    <!-- Demo CSS -->		<link href="{{asset('css/demo-landing.css')}}" rel="stylesheet">
+    <!-- Skin CSS -->		<link id="skinCSS" rel="stylesheet" href="{{asset('css/skin-landing.css')}}">
+
+
+</head>
