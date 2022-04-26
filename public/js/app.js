444fda268eb8cffb29c840d4b7a9c646134d50c9
bes <rtvafk@i.ua>
2022-04-26 13:11:51 +0300

    add css\js for new template && vendor to old resources

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,28 @@
-require('./bootstrap');
-require('./theme');
+// require('./bootstrap');
+// require('./theme');
 
 //Head Libs
-require('./modernizr.min');
+// require('./modernizr.min');
 
 //Global site tag (gtag.js) - Google Analytics
 // require('./gtag')
+require('./jquery/jquery.min');
+require('./jquery.appear/jquery.appear.min');
+require('./jquery.easing/jquery.easing.min');
+require('./jquery.cookie/jquery.cookie.min');
+require('./popper/umd/popper.min');
+require('./bootstrap/bootstrap.min');
+require('./jquery.validation/jquery.validate.min');
+require('./jquery.easy-pie-chart/jquery.easypiechart.min');
+require('./jquery.gmap/jquery.gmap.min');
+require('./lazysizes/lazysizes.min');
+require('./isotope/jquery.isotope.min');
+require('owl.carousel.min');
+require('jquery.magnific-popup.min');
+require('./vide/jquery.vide.min');
+require('./vivus/vivus.min');
+require('./bootstrap-star-rating/js/star-rating.min');
+require('./bootstrap-star-rating/themes/krajee-fas/theme.min');
+require('./jquery.countdown/jquery.countdown.min');
+require('./modernizr.min');
+require('./theme');
